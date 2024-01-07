06e8a2534d8df6f2ade236498c40833893089b03
timur <atpoverload@gmail.com>
2024-01-07 14:16:45 -0700

    adding a powercap meter through old eflect

---
+++ eflect_rapl/src/main/java/eflect/util/Powercap.java
@@ -0,0 +1,78 @@
+package eflect.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+/** Simple wrapper to read powercap's energy with pure Java. */
+public final class Powercap {
+  private static final String POWERCAP_PATH =
+      String.join("/", "/sys", "devices", "virtual", "powercap", "intel-rapl");
+
+  public static final int SOCKET_COUNT = getSocketCount();
+
+  private static final Logger logger = LoggerUtil.getLogger();
+
+  /**
+   * Returns an {@link PowercapSample} populated by parsing the string returned by {@ readNative}.
+   */
+  public static double[][] getEnergyStats() {
+    if (SOCKET_COUNT == 0) {
+      return new double[0][0];
+    }
+    double[][] sample = new double[SOCKET_COUNT][];
+
+    // pull out energy values
+    for (int socket = 0; socket < SOCKET_COUNT; socket++) {
+      sample[socket] = new double[2];
+      sample[socket][0] = readPackage(socket);
+      sample[socket][1] = readDram(socket);
+    }
+
+    return sample;
+  }
+
+  private static int getSocketCount() {
+    try {
+      return (int)
+          Stream.of(new File(POWERCAP_PATH).list()).filter(f -> f.contains("intel-rapl")).count();
+    } catch (Exception e) {
+      logger.fine("couldn't check the socket count; powercap likely not available");
+      return 0;
+    }
+  }
+
+  // TODO: NEED TO LOG THESE!!!!
+  /**
+   * Parses the contents of /sys/devices/virtual/powercap/intel-rapl/intel-rapl:<socket>/energy_uj,
+   * which contains the number of microjoules consumed by the package since boot as an integer.
+   */
+  private static double readPackage(int socket) {
+    String energyFile =
+        String.join("/", POWERCAP_PATH, String.format("intel-rapl:%d", socket), "energy_uj");
+    try (BufferedReader reader = new BufferedReader(new FileReader(energyFile))) {
+      return Double.parseDouble(reader.readLine()) / 1000000;
+    } catch (Exception e) {
+      return 0;
+    }
+  }
+
+  /**
+   * Parses the contents of
+   * /sys/devices/virtual/powercap/intel-rapl/intel-rapl:<socket>/intel-rapl:<socket>:0/energy_uj,
+   * which contains the number of microjoules consumed by the dram since boot as an integer.
+   */
+  private static double readDram(int socket) {
+    String socketPrefix = String.format("intel-rapl:%d", socket);
+    String energyFile =
+        String.join(
+            "/", POWERCAP_PATH, socketPrefix, String.format("%s:0", socketPrefix), "energy_uj");
+    try (BufferedReader reader = new BufferedReader(new FileReader(energyFile))) {
+      return Double.parseDouble(reader.readLine()) / 1000000;
+    } catch (Exception e) {
+      return 0;
+    }
+  }
+}
---
+++ experiments/main/java/flora/experiments/sunflow/renderer/PowercapDriver.java
@@ -0,0 +1,105 @@
+package flora.experiments.sunflow.renderer;
+
+import flora.Machine;
+import flora.Meter;
+import flora.experiments.sunflow.ConfigurableScene;
+import flora.experiments.sunflow.ConfigurableSceneFactory;
+import flora.experiments.sunflow.RenderingConfiguration;
+import flora.experiments.sunflow.RenderingKnobs;
+import flora.experiments.sunflow.image.BufferedImageDisplay;
+import flora.experiments.sunflow.image.ImageDistanceMeter;
+import flora.experiments.sunflow.image.ImageDistanceScore;
+import flora.experiments.sunflow.scenes.ConfigurableFileScene;
+import flora.experiments.sunflow.scenes.CornellBox;
+import flora.knob.meta.RangeConstrainedKnob;
+import flora.meter.Stopwatch;
+import flora.meter.contrib.PowercapEflectMeter;
+import flora.strategy.contrib.ears.CompatNumberProblem;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import javax.imageio.ImageIO;
+import org.sunflow.system.UI;
+import org.um.feri.ears.algorithms.moo.nsga2.D_NSGAII;
+import org.um.feri.ears.problems.StopCriterion;
+import org.um.feri.ears.problems.Task;
+
+final class PowercapDriver {
+  private static final int REFERENCE_TIMEOUT =
+      60 * 60 * 1000; // 1 hour timeout for the reference image (just in case)
+
+  public static void main(String[] args) throws Exception {
+    // Setup the knobs and display
+    UI.verbosity(0);
+    BufferedImageDisplay display = new BufferedImageDisplay();
+    RenderingKnobs knobs = RenderingKnobs.DEFAULT_KNOBS;
+    RenderingConfiguration configuration = RenderingKnobs.DEFAULT_CONFIGURATION;
+    RangeConstrainedKnob[] knobsArray =
+        Arrays.stream(knobs.asArray())
+            .map(RangeConstrainedKnob::new)
+            .toArray(RangeConstrainedKnob[]::new);
+
+    // generate the reference
+    ConfigurableScene scene =
+        args.length == 0
+            ? new CornellBox(knobs, configuration, display, REFERENCE_TIMEOUT)
+            : new ConfigurableFileScene(knobs, configuration, display, REFERENCE_TIMEOUT, args[0]);
+    Instant start = Instant.now();
+    scene.run();
+    int timeOut = (int) (3 * Duration.between(start, Instant.now()).toMillis() / 2);
+    // System.out.println("TIMEOUT IS " + timeOut);
+
+    try {
+      File imageFile = new File("/tmp/flora/reference.png");
+      ImageIO.write(display.getImage(), "png", imageFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    // setup the meters with the reference
+    ImageDistanceMeter imageMeter =
+        new ImageDistanceMeter(display, display.getImage(), ImageDistanceScore.MSE);
+    Map<String, Meter> meters =
+        Map.of(
+            "energy",
+            PowercapEflectMeter.newLocalMeter(4),
+            "runtime",
+            new Stopwatch(),
+            "score",
+            imageMeter);
+
+    // wire everything together
+    Machine machine =
+        new Machine() {
+          @Override
+          public Map<String, Meter> meters() {
+            return new HashMap<>(meters);
+          }
+        };
+
+    scene =
+        args.length == 0
+            ? new CornellBox(knobs, configuration, display, timeOut)
+            : new ConfigurableFileScene(knobs, configuration, display, timeOut, args[0]);
+    CompatNumberProblem problem =
+        new CompatNumberProblem("rendering-problem", new ConfigurableSceneFactory(scene), machine);
+
+    // run the renderer
+    D_NSGAII nsga = new D_NSGAII();
+    nsga.execute(new Task<>(problem, StopCriterion.EVALUATIONS, 10000, 0, 0));
+
+    // int i = 0;
+    // for (BufferedImage image : imageMeter.images) {
+    //   try {
+    //     File imageFile = new File(String.format("/tmp/flora/%d.png", i++));
+    //     ImageIO.write(image, "png", imageFile);
+    //   } catch (IOException e) {
+    //     e.printStackTrace();
+    //   }
+    // }
+  }
+}
