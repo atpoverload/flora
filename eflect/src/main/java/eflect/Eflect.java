01c6621efc2f284c9aa12b249d54b641e4cab894
timur <atpoverload@gmail.com>
2024-01-17 16:48:14 -0700

    another minor fix

--- eflect/src/main/java/eflect/Eflect.java
+++ eflect/src/main/java/eflect/Eflect.java
@@ -39,7 +39,7 @@ public final class Eflect {
             () ->
                 new EnergyAccountant(
                     Rapl.getInstance().getSocketCount(),
-                    /* componentCount= */ 2,
+                    /* componentCount= */ 3,
                     /* wrapAround= */ Rapl.getInstance().getWrapAroundEnergy(),
                     new JiffiesAccountant(
                         Rapl.getInstance().getSocketCount(),
@@ -60,7 +60,7 @@ public final class Eflect {
             () ->
                 new EnergyAccountant(
                     Powercap.SOCKET_COUNT,
-                    /* componentCount= */ 3,
+                    /* componentCount= */ 2,
                     /* wrapAround= */ 0,
                     new JiffiesAccountant(
                         Powercap.SOCKET_COUNT, cpu -> cpu / (CPU_COUNT / Powercap.SOCKET_COUNT))),
