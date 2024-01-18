8229d8b4133381871f1393da9935ff802a163614
timur <atpoverload@gmail.com>
2024-01-17 21:40:50 -0700

    adding some logging to debug energy reporting

--- contrib/main/java/flora/contrib/eflect/EflectMeter.java
+++ contrib/main/java/flora/contrib/eflect/EflectMeter.java
@@ -1,9 +1,12 @@
 package flora.contrib.eflect;
 
-import flora.util.LoggerUtil;
 import eflect.Eflect;
 import eflect.data.EnergyFootprint;
+import eflect.util.TimeUtil;
 import flora.Meter;
+import flora.util.LoggerUtil;
+import java.time.Duration;
+import java.time.Instant;
 
 public class EflectMeter implements Meter {
   private final Eflect eflect;
@@ -27,12 +30,20 @@ public class EflectMeter implements Meter {
   @Override
   public double read() {
     double energy = 0;
+    Instant start = Instant.MAX;
+    Instant end = Instant.MIN;
     for (EnergyFootprint footprint : eflect.read()) {
+      start = TimeUtil.min(footprint.start, start);
+      end = TimeUtil.min(footprint.end, end);
       if (footprint.energy > 0) {
         energy += footprint.energy;
       }
     }
-    LoggerUtil.getLogger().fine("eflect reports %fJ");
+    LoggerUtil.getLogger()
+        .fine(
+            String.format(
+                "eflect reports %fJ over %s to %s (%s)",
+                energy, start, end, Duration.between(start, end)));
     return energy;
   }
 }
